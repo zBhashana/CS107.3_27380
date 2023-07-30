0fc8e3535f8447104c6d5dfb3c51ab35e22bb7c3
Gunarathna R T B R <132426936+zBhashana@users.noreply.github.com>
2023-07-30 15:46:54 +0530

    Add files via upload

---
+++ C# lab 05/C# lab 05_Q3/C# lab 05_Q3/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__lab_05_Q3
+{
+    class CalculateValues
+    {
+        public int Addition(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public int Subtraction(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public int Multiplication(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public int Division(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+    }
+
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            CalculateValues calculateValues = new CalculateValues();
+
+            int choice = 0;
+            int num1 = 0;
+            int num2 = 0;
+
+            Console.WriteLine("Choose an operation:");
+            Console.WriteLine("1. Addition");
+            Console.WriteLine("2. Subtraction");
+            Console.WriteLine("3. Multiplication");
+            Console.WriteLine("4. Division");
+
+            choice = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter two numbers:");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("The answer is: " + calculateValues.Addition(num1, num2));
+                    break;
+                case 2:
+                    Console.WriteLine("The answer is: " + calculateValues.Subtraction(num1, num2));
+                    break;
+                case 3:
+                    Console.WriteLine("The answer is: " + calculateValues.Multiplication(num1, num2));
+                    break;
+                case 4:
+                    Console.WriteLine("The answer is: " + calculateValues.Division(num1, num2));
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice!");
+                    break;
+            }
+        }
+    }
+}
---
+++ C# lab 05/C# lab 05_Q4/C# lab 05_Q4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__lab_05_Q4
+{
+    public class MyClass
+    {
+        private void sayHello()
+        {
+            Console.WriteLine("Hello, world!");
+        }
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Create an object of the MyClass class.
+            var myClass = new MyClass();
+            // Try to access the sayHello() method.
+            //myClass.sayHello(); // This will not compile.
+        }
+    }
+    public class MyClass
+    {
+        public void sayHello()
+        {
+            Console.WriteLine("Hello, world!");
+        }
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Create an object of the MyClass class.
+            var myClass = new MyClass();
+            // Access the sayHello() method.
+            myClass.sayHello();
+        }
+    }
+}
